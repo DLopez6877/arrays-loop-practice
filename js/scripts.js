3098ec1111c3a17d1de1795ee1180db9bc4f5ab3
Daniel <dlopez6877@gmail.com>
2017-05-01 22:41:08 -0700

    add deck of cards and add if statement for blank grocery items

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,17 @@
 // front end below this line
 $(function() {
 
+  // create grocery list when submitting form
   $("#groceryForm").submit(function(event){
     var groceries = ["grocery1", "grocery2", "grocery3", "grocery4", "grocery5", "grocery6"];
     var newArray = [];
-    
+
     groceries.forEach(function(grocery){
       var groceryInput = $("input#" + grocery).val();
+      console.log(groceryInput);
+      if (groceryInput === "") {
+        groceryInput = "(Blank)";
+      }
       newArray.push(groceryInput);
     });
 
@@ -16,10 +21,31 @@ $(function() {
     newArray.forEach(function(grocerySorted){
       $("#grocery-list").append('<li>' + grocerySorted.toUpperCase() + '</li>');
       $("#groceryForm").hide();
-      $("#grocery-list").show();
+      $(".grocery-list").show();
     });
 
     event.preventDefault();
+  });
+
+  // create a deck of cards
+  $(".cards-here").click(function(event) {
+    var cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+    var suits = ["Spade", "Hearts", "Clubs", "Diamonds"];
+    var completedCards = [];
+
+    cards.forEach(function(card){
+      suits.forEach(function(suit){
+        completedCards.push(card + " of " + suit );
+      });
+    });
+
+    completedCards = completedCards.sort();
+
+    completedCards.forEach(function(completedCard) {
+      $("#deck").append("<li>" + completedCard + "</li>");
+    });
+
+    $("#deck").toggle();
   });
 
 });
